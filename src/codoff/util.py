a793291505dd0fabc08155d2a1f5bae5c4b502bd
Rauf Salamzade <salamzader@gmail.com>
2024-05-02 11:34:29 -0500

    Update util.py

--- src/codoff/util.py
+++ src/codoff/util.py
@@ -1,6 +1,28 @@
 import os
 import traceback
 
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+        """
+        Function from: https://stackoverflow.com/questions/3173320/text-progress-bar-in-terminal-with-block-characters
+        Call in a loop to create terminal progress bar
+        @params:
+                iteration   - Required  : current iteration (Int)
+                total       - Required  : total iterations (Int)
+                prefix      - Optional  : prefix string (Str)
+                suffix      - Optional  : suffix string (Str)
+                decimals    - Optional  : positive number of decimals in percent complete (Int)
+                length      - Optional  : character length of bar (Int)
+                fill        - Optional  : bar fill character (Str)
+                printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+        """
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + '-' * (length - filledLength)
+        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+        # Print New Line on Complete
+        if iteration == total:        
+                print()
+
 def parseCDSCoord(str_gbk_loc):
         try:
                 start = None

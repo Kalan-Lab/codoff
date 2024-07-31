2194206e18c6f32578ed06035ceb6512e6559125
Rauf Salamzade <salamzader@gmail.com>
2024-07-30 20:52:06 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from setuptools import setup
+import os
+
+if __name__ == "__main__":
+    setup(long_description=open('README.md').read(),
+    long_description_content_type='text/markdown')

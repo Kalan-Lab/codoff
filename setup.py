58eb8174af83d4ea2aa14532dfd9040f842a050e
Rauf Salamzade <salamzader@gmail.com>
2024-07-30 20:55:35 -0500

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-from setuptools import setup
-import os
-
-if __name__ == "__main__":
-    setup(long_description=open('README.md').read(),
-    long_description_content_type='text/markdown')
